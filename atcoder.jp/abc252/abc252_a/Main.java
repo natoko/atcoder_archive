bb5f8123d1e27decff4e7adb80281bdd45d8f907
Risa Katsumata <natoko.1230@gmail.com>
2022-06-04 15:55:21 +0900

    auto commit

---
+++ atcoder.jp/abc252/abc252_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.*;
+public class Main {
+	public static int N;
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		N = Integer.parseInt(scan.next());
+		scan.close();
+
+		char n = (char)N;
+
+		System.out.println(n);
+
+	}
+
+
+}
